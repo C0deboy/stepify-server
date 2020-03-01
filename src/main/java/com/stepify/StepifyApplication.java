240d65e7c78e4bf5fecef8b2f0a6a6d61de4ff51
C0deboy <ellucky4@gmail.com>
2020-03-01 14:57:32 +0100

    open broswer on startup

--- src/main/java/com/stepify/StepifyApplication.java
+++ src/main/java/com/stepify/StepifyApplication.java
@@ -1,5 +1,7 @@
 package com.stepify;
 
+import java.io.IOException;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,10 @@ public class StepifyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StepifyApplication.class, args);
+    }
+
+    @PostConstruct
+    public void start() throws IOException {
+        new ProcessBuilder().command("cmd.exe", "/c", "start", "http://localhost:8000/login").start();
     }
 }
